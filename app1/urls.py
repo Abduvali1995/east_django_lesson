6efbf1f42816c3c58a9a6b54338c1c1444cf1403
Abduvali1995 <johnhacker519@gmail.com>
2024-01-22 17:15:28 +0500

    initial project

---
+++ app1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import first_view,second_view,three_view,four_view
+
+urlpatterns =  [
+    path('', first_view, name = 'first-view'),
+    path('second_view', second_view, name = 'second-view'),
+    path('three_view', three_view, name='three-view'),
+    path('four_view', four_view, name='three-view'),
+]
---
+++ app1/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def first_view(requets):
+    html = '''
+        <h1> Assalamu alaykum bzining web sayitimizga hush kelibsiz </h1>
+        <p>bugun juda qiziq narsa o`rganyapmiz</p>
+        <a href="second_view"> <h2>Python</h2> </a>
+         <a href="three_view"> <h2>Django</h2> </a>
+    '''
+    return HttpResponse(html)
+
+def second_view(request):
+    html = '''
+        <h1> Bu ikkinchi sahifa python haqida ma`lumot beradi </h1>
+        <p>  python high level programming language python was created by G.Rossum</p>
+        <a href="../"><h2>Home</h2></a>
+    '''
+    return HttpResponse(html)
+
+def three_view(request):
+    html = '''
+          <h1> Bu 3- sahifa Django haqida ma`lumot beradi </h1>
+          <p>  Django high level python programming framework python was created by G.Rossum</p>
+          <a href="../"><h2>Home</h2></a>
+          <a href="four_view"><h2>C++</h2></a>
+      '''
+    return HttpResponse(html)
+
+def four_view(request):
+    html = '''
+             <h1> Bu 4- sahifa C++ haqida ma`lumot beradi </h1>
+             <p>  C++ high level  programming  python was created by G.Rossum</p>
+             <a href="three_view"><h2>Home</h2></a>
+         '''
+    return HttpResponse(html)
